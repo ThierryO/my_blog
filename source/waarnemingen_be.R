70a7f074dffb8c2049edb69b2db3a4b481b07079
ThierryO <Thierry.Onkelinx@inbo.be>
2017-06-16 21:19:32 +0200

    reformat metadata of observations

--- source/waarnemingen_be.R
+++ source/waarnemingen_be.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(lubridate)
 
 read_observation <- function(id){
   lapply(
@@ -8,6 +9,19 @@ read_observation <- function(id){
       read.delim(fileEncoding = "UTF-16", stringsAsFactors = FALSE)
     }
   ) %>%
-    bind_rows()
+    bind_rows() %>%
+    transmute(
+      route_id = Streeplijst,
+      observation_id = Waarneming,
+      time = paste(Datum, Tijd) %>%
+        as.POSIXct(tz = "CEST"),
+      starttime = paste(Datum, Starttijd.bezoek) %>%
+        as.POSIXct(tz = "CEST"),
+      endtime = paste(Datum, Eindtijd) %>%
+        as.POSIXct(tz = "CEST"),
+      starttime = starttime + ifelse(time < starttime, -24 * 3600, 0),
+      endtime = endtime + ifelse(endtime < time, 24 * 3600, 0),
+      species = factor(Wetenschappelijke.naam),
+      count = Aantal
+    )
 }
-
